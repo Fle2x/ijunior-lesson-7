5ff4edd58f1b8e9e5f12ad0f1f53163a1d069fb8
Fle2x <dotsfam87@Mail.ru>
2020-10-01 11:32:37 +0500

    2D платформер
    Создать простой 2D платформер в котором реализовать:
    -Управление персонажем(бег, прыжки)
    -Анимацию бега(прыжок не обязательно)
    -Простые враги который будут патрулировать небольшой участок
    -Создать спавн монеток, которые можно собирать.

---
+++ ijunior-lesson-7/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    private float _maxLimitEnemyPositionX = 5;
+    private float _spawnInterval = 2;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRandomEnemy());
+    }
+    IEnumerator SpawnRandomEnemy()
+    {
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-_maxLimitEnemyPositionX, _maxLimitEnemyPositionX), 5, 0);
+            Instantiate(_enemyPrefab, spawnPosition, _enemyPrefab.transform.rotation);
+            yield return new WaitForSeconds(_spawnInterval); 
+        }
+    }
+}
